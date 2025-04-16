5709df65580f649af931568febca37c194544167
Luan Pimenta Fernandes <130945489+pimentaluan@users.noreply.github.com>
2025-04-16 11:08:05 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-36yi$np21xuxi+wj(ugnp2)pnh)o&$8^+twtuh)tq=e0cixec3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devtasks.onrender.com']
 
 
 # Application definition
